bdf26ec5d3f6a4c4ff90dd7eec64226d0590ce61
Yasmeen Roumie <y4smeen@gmail.com>
2016-02-09 10:22:31 -0500

    canvas

---
+++ canvas/canvas.js
@@ -0,0 +1,6 @@
+var c = document.getElementById("c");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(100,100,500,500);
+ctx.strokeStyle = "#0000ff";
+ctx.strokeRect(100,100,500,500);
