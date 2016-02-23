898d6e35ae88a718a8cab4c4a369e90038c9a8e9
Yasmeen Roumie <y4smeen@gmail.com>
2016-02-23 07:44:54 -0500

    circle

--- hw02/animate.js
+++ hw02/animate.js
@@ -1,11 +1,29 @@
+/*
+Yasmeen Roumie
+SoftDev2 pd 3
+HW02 -- Dot, Dot, Dot
+2016-02-22
+*/
+
 var c = document.getElementById("canvas"); // Canvas
-var button = document.getElementById("clear"); // Clear button
+var button = document.getElementById("toggle"); // Clear button
+var clear = document.getElementById("clear"); // Clear button
 var ctx = canvas.getContext("2d"); // Stuff on canvas
 
-var radius = 0;
+function clearCanvas(event){
+  event.preventDefault();
+  ctx.closePath();
+  ctx.beginPath();
+  ctx.clearRect(0, 0, c.width, c.height); // Clear rectangle that covers the canvas
+}
+
+var radius = 10;
 var growing = true;
-var drawDot = function(){
+ctx.fillStyle = "red";
+
+var drawDot = function() {
   ctx.clearRect(0, 0, c.width, c.height);
+
   if (growing){
     radius = radius + 1;
   } else {
@@ -17,9 +35,16 @@ var drawDot = function(){
   else if (radius == 0){
     growing = true;
   }
+
   ctx.beginPath();
   ctx.arc(c.width/2, c.height/2, radius, 0, 2*Math.PI);
   ctx.stroke();
   ctx.fill();
   window.requestAnimationFrame(drawDot);
 };
+
+//drawDot();
+//button.addEventListener("click", clearCanvas);
+
+button.addEventListener("click", drawDot);
+clear.addEventListener("click", clearCanvas);
