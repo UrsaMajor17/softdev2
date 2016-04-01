51aafd683b0c8381b75f559fc2b12a9fdffe3182
y4smeen <y4smeen@gmail.com>
2016-04-01 10:00:44 -0400

    start closure

--- hw06/closure.py
+++ hw06/closure.py
@@ -3,9 +3,14 @@
 # HW 6 -- Closure
 # 2016-04-01
 
-def f(x):
-    def g(y):
-        return x + y
-    return g
+# TASK: 
+# Write a closure with outer function named 'repeat'
+# such that the function calls shown below will yield the outputs shown.
 
-print f(1)(2)
+
+def r1(x):
+    repeated = ""
+    for 
+print r1(2) # -> hellohello
+print r2(2) # -> goodbyegoodbye
+print repeat('cool')(3) # -> coolcoolcool
