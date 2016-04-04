e60cb39f907f33b010262c9a7eb77ffb70d79a13
Yasmeen Roumie <y4smeen@gmail.com>
2016-04-04 09:48:28 -0400

    .

--- hw06/closure.py
+++ hw06/closure.py
@@ -3,14 +3,16 @@
 # HW 6 -- Closure
 # 2016-04-01
 
-# TASK: 
+# TASK:
 # Write a closure with outer function named 'repeat'
 # such that the function calls shown below will yield the outputs shown.
 
 
-def r1(x):
-    repeated = ""
-    for 
-print r1(2) # -> hellohello
-print r2(2) # -> goodbyegoodbye
-print repeat('cool')(3) # -> coolcoolcool
+def repeat(string):
+    def times(num):
+        return string*num
+    return times
+
+#print r1(2) # -> hellohello
+#print r2(2) # -> goodbyegoodbye
+print repeat('omg')(5) # -> coolcoolcool
